9f7e73339deb30d7c5d22c68bb75bed69953b273
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-07-12 20:56:10 +0200

    update server file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
+const PATH_PROJECT = '/dist/draughts';
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
+const server = http.createServer(app);
 
-app.use(express.static(__dirname + '/dist/draughts'));
+app.use(express.static(__dirname + PATH_PROJECT));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/draughts/index.html'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, PATH_PROJECT, index.html)));
+
+server.listen(process.env.PORT || 8081, () => {
+  if (!process.env.PORT) {
+    console.log('Running with Express... http://localhost:8081/');
+  }
 });
-
-app.listen(process.env.PORT || 8080);
