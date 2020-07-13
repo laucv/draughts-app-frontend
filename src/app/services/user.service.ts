19c2ba99f51cc7f94c9de5727b6aa662c830dda3
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-07-13 17:45:37 +0200

    cambios para que funcione profile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,37 +2,29 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {UserModel} from "../models/User.model";
 import {Observable} from "rxjs";
-import {environment} from '../../environments/environment';
 
 @Injectable()
 export class UserService {
 
-  static API_END_POINT = environment.API;
-  private headers: HttpHeaders;
+  private url: string = 'http://localhost:4600';
 
   constructor(private http: HttpClient) {
-    this.headers = new HttpHeaders();
-    this.setHeaders();
   }
 
   getUsers(): Observable<UserModel[]> {
-    return this.http.get<UserModel[]>(UserService.API_END_POINT + '/users', {
-      headers: this.headers
-    });
+    return this.http.get<UserModel[]>(this.url + '/users');
   }
 
   login(email: string, password: string): Observable<string> {
     const user = '{' +
       '"email": "' + email + '", ' +
       '"password":"' + password + '"}';
-    return this.http.post<string>(UserService.API_END_POINT + '/users/login', JSON.parse(user), {
-      headers: this.headers
-    });
+    return this.http.post<string>(this.url + '/users/login', JSON.parse(user));
   }
 
   getUserProfile(): Observable<UserModel> {
-    return this.http.get<UserModel>(UserService.API_END_POINT + '/users/profile', {
-      headers: this.headers
+    return this.http.get<UserModel>(this.url + '/users/profile', {
+      headers: this.getHeaders()
     });
   }
 
@@ -49,14 +41,11 @@ export class UserService {
       '"username": "' + username + '", ' +
       '"email": "' + email + '", ' +
       '"password":"' + password + '"}';
-    return this.http.post<UserModel>(UserService.API_END_POINT + '/users', JSON.parse(user), {
-      headers: this.headers
-    });
+    return this.http.post<UserModel>(this.url + '/users', JSON.parse(user));
   }
 
   saveToken(token: string) {
     window.sessionStorage.setItem('token', token);
-    this.setAuthTokenHeader();
   }
 
   getToken(): string {
@@ -64,25 +53,21 @@ export class UserService {
   }
 
   deleteUser(): Observable<UserModel> {
-    return this.http.delete<UserModel>(UserService.API_END_POINT + '/users/profile', {
-      headers: this.headers
+    return this.http.delete<UserModel>(this.url + '/users/profile', {
+      headers: this.getHeaders()
     });
   }
 
-  setHeaders(): HttpHeaders {
-    return this.headers.set('Access-Control-Allow-Origin', '*');
-  }
-
-  setAuthTokenHeader(): HttpHeaders {
-    return this.headers.set('auth-token', this.getToken());
+  getHeaders(): HttpHeaders {
+    return new HttpHeaders().set('auth-token', this.getToken());
   }
 
   updateUser(username: string): Observable<UserModel> {
     const user = '{' +
       '"username": "' + username + '"}';
 
-    return this.http.put<UserModel>(UserService.API_END_POINT + '/users/profile', JSON.parse(user), {
-      headers: this.headers
+    return this.http.put<UserModel>(this.url + '/users/profile', JSON.parse(user), {
+      headers: this.getHeaders()
     });
   }
 
