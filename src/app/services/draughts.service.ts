4688bc3856d1d9a23c01cf29be7eb588619ede5c
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-07-13 15:58:23 +0200

    add headers in services

--- src/app/services/draughts.service.ts
+++ src/app/services/draughts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {DraughtsModel} from '../models/Draughts.model';
 import {UserService} from './user.service';
 import {environment} from '../../environments/environment';
@@ -10,16 +10,23 @@ import {environment} from '../../environments/environment';
 export class DraughtsService {
 
   static API_END_POINT = environment.API;
-
+  private headers: HttpHeaders;
+  
   constructor(private http: HttpClient, private userService: UserService) {
+    this.headers = new HttpHeaders();
+    this.setHeaders();
   }
 
   get(){
-    return this.http.get<DraughtsModel[]>(DraughtsService.API_END_POINT + '/draughts');
+    return this.http.get<DraughtsModel[]>(DraughtsService.API_END_POINT + '/draughts', {
+      headers: this.headers
+    });
   }
 
   getAllByUser(userId: string){
-    return this.http.get<DraughtsModel[]>(DraughtsService.API_END_POINT + '/draughts/user/' + userId);
+    return this.http.get<DraughtsModel[]>(DraughtsService.API_END_POINT + '/draughts/user/' + userId, {
+      headers: this.headers
+    });
   }
 
   post(game_name: string, board: string, turn: number){
@@ -29,14 +36,18 @@ export class DraughtsService {
       '"board": "' + board + '", ' +
       '"creator": "' + creator + '", ' +
       '"turn":"' + turn + '"}';
-    return this.http.post<DraughtsModel>(DraughtsService.API_END_POINT + '/draughts', JSON.parse(game));
+    return this.http.post<DraughtsModel>(DraughtsService.API_END_POINT + '/draughts', JSON.parse(game), {
+      headers: this.headers
+    });
   }
 
   put(game_name: string, board: string, turn: number){
     const game = '{' +
       '"board": "' + board + '", ' +
       '"turn":"' + turn + '"}';
-    return this.http.put<DraughtsModel>(DraughtsService.API_END_POINT + '/draughts/' + game_name, JSON.parse(game));
+    return this.http.put<DraughtsModel>(DraughtsService.API_END_POINT + '/draughts/' + game_name, JSON.parse(game), {
+      headers: this.headers
+    });
   }
 
   parseJwt () {
@@ -48,5 +59,9 @@ export class DraughtsService {
     }).join(''));
 
     return JSON.parse(jsonPayload);
+  }
+
+  setHeaders(): HttpHeaders {
+    return this.headers.set('Access-Control-Allow-Origin', '*');
   }
 }
